82643d4f228c48ab85efbe2ea6528aa188555cd2
Jonathon Vogel <jonathon.vogel@gmail.com>
2014-03-29 15:36:15 -0400

    Add 2to3 fix to patch bcrypt

--- setup_app.py
+++ setup_app.py
@@ -1,13 +1,22 @@
-import sqlite3
 import contextlib
+import os
+import subprocess
+import sqlite3
 
 import config
 
 def init_db():
-    with contextlib.closing(sqlite3.connect(config.database)) as db:
-        with open('schema.sql') as schema:
-            db.cursor().executescript(schema.read())
-        db.commit()
+    if os.path.exists(config.database):
+        print('Database already exists, skipping rebuild.......')
+    else:
+        with contextlib.closing(sqlite3.connect(config.database)) as db:
+            with open('schema.sql') as schema:
+                db.cursor().executescript(schema.read())
+            db.commit()
+
+def convert_flask_bcrypt():
+    subprocess.call(['2to3', '-w', 'venv/lib/python3.3/site-packages/flaskext/bcrypt.py'])
 
 if __name__ == '__main__':
     init_db()
+    convert_flask_bcrypt()
