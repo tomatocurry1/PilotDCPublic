5e5bc6c9f74da1ef643f9d74309af3a14c274e17
Austin Ly <austinly1997@yahoo.com>
2014-03-29 19:32:44 -0400

    Welcome page
    FINISHED BITCHES

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 	$("body").hide().fadeIn("slow");
+	
+	$("#login-button").click(function() {
+		$("#email").slideUp("fast");
+		$("#latitude").slideUp("fast");
+		$("#longitude").slideUp("fast");
+		$("#login-to-register").addClass("hidden");
+		$("#register-to-login").removeClass("hidden");
+		$("#username").animate({"margin-top": "100px"}, "fast");
+		$("#regbutton").text("Log in");
+
+	});
+
+	$("#register-button").click(function() {
+		$("#email").slideDown("fast");
+		$("#latitude").slideDown("fast");
+		$("#longitude").slideDown("fast");
+		$("#login-to-register").removeClass("hidden");
+		$("#register-to-login").addClass("hidden");
+		$("#username").animate({"margin-top": "60px"}, "fast");
+		$("#regbutton").text("Register");
+
+	})
 });
